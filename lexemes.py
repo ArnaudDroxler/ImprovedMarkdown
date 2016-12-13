a55ccf203870cfffaed2f0b15ce391e95b15a79d
arnaud droxler <arnaud.droxler@gmail.com>
2016-12-13 16:32:09 +0100

    ajout de certains lexemes

--- lexemes.py
+++ lexemes.py
@@ -3,39 +3,63 @@
 import ply.lex as lex
 
 tokens = (
-	'NUMBER',
-	'ADD_OP',
-	'MUL_OP'
+	'BOLD',
+	'ITALIC',
+	'FLUO',
+      'LINK',
+      'REFERENCE',
+      'IMAGE',
+      'WORD',
+      'NUMBER',
+      'SEPARATION_LINE'
 )
 
-literals = '()'
-
-def t_ADD_OP(t):
-	r'\+|-'
+def t_BOLD(t):
+	r'\*\*.*\*\*'
 	return t
 
-def t_MUL_OP(t):
-	r'\*|/'
+def t_ITALIC(t):
+	r'\*.*\*'
+	return t
+ 
+def t_FLUO(t):
+	r'!.*!'
 	return t
 
+def t_LINK(t):
+    r'\[.*\]\({1}.*\){1}'
+    return t
+    
+def t_IMAGE(t):
+    r'!{1}\[{1}.*\]{1}\({1}.*\){1}'
+    return t
+
+def t_REFERENCE(t):
+    r'\w*\{{1}.*\}{1}'
+    return t
+    
 def t_NUMBER(t):
-	r'\d+(\.\d+)?'
-	try:
-		t.value = float(t.value)
-	except ValueError:
-		print ("Line %d: Problem while parsing %s!" % (t.lineno,t.value))
-		t.value = 0
-	return t
+    r'\d+'
+    t.value = int(t.value)
+    return t
 
+def t_WORD(t):
+    r'\w+[,.?:;&\-!?]?'
+    return t
+
+def t_SEPARATION_LINE(t):
+    r'__{2,}\s{0,}\n'
+    return t
+    
 def t_newline(t):
 	r'\n+'
 	t.lexer.lineno += len(t.value)
 
-t_ignore  = ' \t'
-
 def t_error(t):
 	print ("Illegal character '%s'" % t.value[0])
 	t.lexer.skip(1)
+
+t_ignore  = ' \t'
 
 lex.lex()
 
