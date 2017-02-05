4179415d732b681c00ee86ef296d068630e947cc
axelroy <royaxe@gmail.com>
2017-02-05 14:01:29 +0100

    Ajout de la récursion dans la grammaire, premier test

--- parser.py
+++ parser.py
@@ -5,42 +5,45 @@ import ply.yacc as yacc
 import AST
 
 from lexemes import tokens
-   
+
 def p_document(p):
     ''' document : paragraph sentence
     '''
-    p[0] = AST.DocumentNode([p[1]]+p[2].children)     
-    
+    p[0] = AST.DocumentNode([p[1]]+p[2].children)
+
 def p_sentence(p):
-    ''' sentence : WORD 
-    | tag'''
-    p[0] = AST.SentenceNode(p[1])       
-  
+    ''' sentence : WORD sentence
+        | tag sentence
+        | WORD sentence tag sentence
+        | WORD
+        | tag'''
+    p[0] = AST.SentenceNode(p[1])
+
 def p_paragraph(p):
     '''paragraph : PARAGRAPHE '''
-    p[0] = AST.ParagraphNode(p[1])  
+    p[0] = AST.ParagraphNode(p[1])
 
 def p_tag(p):
-    ''' tag : BOLD 
-    | FLUO 
-    | LINK 
-    | REFERENCE 
-    | IMAGE 
-    | SEPARATION_LINE 
-    | UL1 
-    | UL2 
-    | UL3 
-    | ITALIC 
-    | OL1 
-    | OL2 
-    | OL3 
-    | TITLE1 
-    | TITLE2 
-    | TITLE3 
+    ''' tag : BOLD
+        | FLUO
+        | LINK
+        | REFERENCE
+        | IMAGE
+        | SEPARATION_LINE
+        | UL1
+        | UL2
+        | UL3
+        | ITALIC
+        | OL1
+        | OL2
+        | OL3
+        | TITLE1
+        | TITLE2
+        | TITLE3
     '''
-    p[0] = AST.TagNode(p[1]) 
-    
-   
+    p[0] = AST.TagNode(p[1])
+
+
 def p_error(p):
     if p:
         print ("Syntax error in line %d" % p.lineno)
